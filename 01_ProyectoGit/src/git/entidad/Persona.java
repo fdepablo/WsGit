f2180c62549c4ea084c10b0aa3f2afe2dae10dad
Felix de Pablo <f.depablo.lobo@gmail.com>
2022-01-23 21:51:56 +0100

    creada la entidad Persona

---
+++ 01_ProyectoGit/src/git/entidad/Persona.java
@@ -0,0 +1,20 @@
+package git.entidad;
+
+public class Persona {
+	private String nombre;
+	private int edad;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+}
