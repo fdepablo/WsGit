668f3a73fa2e992237eb6ef9883488cf762044d0
Felix de Pablo <f.depablo.lobo@gmail.com>
2022-01-30 18:14:22 +0100

    add salida por pantalla

--- 01_ProyectoGit/src/git/main/Main.java
+++ 01_ProyectoGit/src/git/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos a nuestras versiones con Git");
-
+		System.out.println("Esto es otro cambio en el proyecto");
 	}
 
 }
