4619e02c5a8658db501534959e7568800152912d
Felix de Pablo <f.depablo.lobo@gmail.com>
2022-01-30 18:35:55 +0100

    creada persona e imprimida por pantalla

--- 01_ProyectoGit/src/git/main/Main.java
+++ 01_ProyectoGit/src/git/main/Main.java
@@ -1,10 +1,18 @@
 package git.main;
 
+import git.entidad.Persona;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos a nuestras versiones con Git");
 		System.out.println("Esto es otro cambio en el proyecto");
+		
+		Persona p = new Persona();
+		p.setNombre("Steve Rogers");
+		p.setEdad(45);
+		
+		System.out.println(p);
 	}
 
 }
