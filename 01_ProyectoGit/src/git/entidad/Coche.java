a69ffad3520fe2a374b856877a53a05d31b98824
Felix de Pablo <f.depablo.lobo@gmail.com>
2022-01-30 18:31:42 +0100

    creada la clase coche

---
+++ 01_ProyectoGit/src/git/entidad/Coche.java
@@ -0,0 +1,27 @@
+package git.entidad;
+
+public class Coche {
+	private String matricula;
+	private String marca;
+	private String modelo;
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	
+}
